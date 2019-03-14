f4b609e94094adbfd837a2df67100cf9175803ef
login256 <ahaloer@163.com>
2019-03-14 20:58:54 +0800

    readelf finish

--- include.mk
+++ include.mk
@@ -4,7 +4,7 @@
 # Written By Zhu Like ( zlike@cse.buaa.edu.cn )
 
 
-CROSS_COMPILE :=  bin/mips_4KC-
+CROSS_COMPILE :=  /OSLAB/compiler/usr/bin/mips_4KC-
 CC			  := $(CROSS_COMPILE)gcc
 CFLAGS		  := -O -G 0 -mno-abicalls -fno-builtin -Wa,-xgot -Wall -fPIC
 LD			  := $(CROSS_COMPILE)ld
--- readelf/readelf.c
+++ readelf/readelf.c
@@ -55,13 +55,18 @@ int readelf(u_char *binary, int size)
         // check whether `binary` is a ELF file.
         if (size < 4 || !is_elf_format(binary)) {
                 printf("not a standard elf format\n");
-                return 0;
+                return -1;
         }
 
         // get section table addr, section header number and section header size.
-
+		ptr_sh_table = binary + ehdr -> e_shoff;
+		sh_entry_count = ehdr -> e_shnum;
+		sh_entry_size = ehdr -> e_shentsize;
         // for each section header, output section number and section addr.
-
+		for (Nr = 1; Nr <= sh_entry_count; Nr++) {
+			shdr = (Elf32_Shdr *)(ptr_sh_table + Nr * sh_entry_size);
+			printf("%d:0x%x\n", Nr, shdr->sh_addr);
+		}
 
         return 0;
 }
