e7c269061a38d0c4a4987f42726040b36bf2af7c
login256 <ahaloer@163.com>
2019-03-18 14:55:01 +0800

    finish extra

--- readelf/readelf.c
+++ readelf/readelf.c
@@ -39,6 +39,18 @@ int is_elf_format(u_char *binary)
  *   Return 0 if success. Otherwise return < 0.
  *   If success, output address of every section in ELF.
  */
+
+#ifndef EI_DATA
+#define EI_DATA 5
+#endif
+
+#define REVERSE_32(n_n) \
+( (((n_n) & 0xff) << 24) | (((n_n) & 0xff00) << 8) | (((n_n)  >> 8) & 0xff00) | (((n_n) >> 24) & 0xff) )
+
+
+#define REVERSE_16(n_n) \
+( (((n_n) & 0xff) << 8) | (((n_n)  >> 8) & 0xff) )
+
 int readelf(u_char *binary, int size)
 {
         Elf32_Ehdr *ehdr = (Elf32_Ehdr *)binary;
@@ -58,16 +70,33 @@ int readelf(u_char *binary, int size)
                 return -1;
         }
 
-        // get section table addr, section header number and section header size.
-		ptr_sh_table = binary + ehdr -> e_shoff;
-		sh_entry_count = ehdr -> e_shnum;
-		sh_entry_size = ehdr -> e_shentsize;
-        // for each section header, output section number and section addr.
-		for (Nr = 1; Nr <= sh_entry_count; Nr++) {
-			shdr = (Elf32_Shdr *)(ptr_sh_table + Nr * sh_entry_size);
-			printf("%d:0x%x\n", Nr, shdr->sh_addr);
+		char elf_Data = ehdr -> e_ident[EI_DATA];
+
+		if (elf_Data == 1)
+		{
+			// get section table addr, section header number and section header size.
+			ptr_sh_table = binary + ehdr -> e_shoff;
+			sh_entry_count = ehdr -> e_shnum;
+			sh_entry_size = ehdr -> e_shentsize;
+			// for each section header, output section number and section addr.
+			for (Nr = 0; Nr < sh_entry_count; Nr++) {
+				shdr = (Elf32_Shdr *)(ptr_sh_table + Nr * sh_entry_size);
+				printf("%d:0x%x\n", Nr, shdr->sh_addr);
+			}
+		}
+		else
+		{
+			// get section table addr, section header number and section header size.
+			ptr_sh_table = binary + REVERSE_32(ehdr -> e_shoff);
+			sh_entry_count = REVERSE_16(ehdr -> e_shnum);
+			sh_entry_size = REVERSE_16(ehdr -> e_shentsize);
+			// for each section header, output section number and section addr.
+			for (Nr = 0; Nr < sh_entry_count; Nr++) {
+				shdr = (Elf32_Shdr *)(ptr_sh_table + Nr * sh_entry_size);
+				printf("%d:0x%x\n", Nr, REVERSE_32(shdr->sh_addr));
+			}
 		}
 
-        return 0;
+		return 0;
 }
 
