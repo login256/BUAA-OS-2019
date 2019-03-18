c42ee5cfa6ad345d004cfc54d9ba601d6f3fef76
login256 <ahaloer@163.com>
2019-03-18 14:17:56 +0800

    first lab1-1

--- readelf/readelf.c
+++ readelf/readelf.c
@@ -45,11 +45,11 @@ int readelf(u_char *binary, int size)
 
         int Nr;
 
-        Elf32_Shdr *shdr = NULL;
+        Elf32_Phdr *phdr = NULL;
 
-        u_char *ptr_sh_table = NULL;
-        Elf32_Half sh_entry_count;
-        Elf32_Half sh_entry_size;
+        u_char *ptr_ph_table = NULL;
+        Elf32_Half ph_entry_count;
+        Elf32_Half ph_entry_size;
 
 
         // check whether `binary` is a ELF file.
@@ -59,13 +59,13 @@ int readelf(u_char *binary, int size)
         }
 
         // get section table addr, section header number and section header size.
-		ptr_sh_table = binary + ehdr -> e_shoff;
-		sh_entry_count = ehdr -> e_shnum;
-		sh_entry_size = ehdr -> e_shentsize;
+		ptr_ph_table = binary + ehdr -> e_phoff;
+		ph_entry_count = ehdr -> e_phnum;
+		ph_entry_size = ehdr -> e_phentsize;
         // for each section header, output section number and section addr.
-		for (Nr = 1; Nr <= sh_entry_count; Nr++) {
-			shdr = (Elf32_Shdr *)(ptr_sh_table + Nr * sh_entry_size);
-			printf("%d:0x%x\n", Nr, shdr->sh_addr);
+		for (Nr = 0; Nr < ph_entry_count; Nr++) {
+			phdr = (Elf32_Phdr *)(ptr_ph_table + Nr * ph_entry_size);
+			printf("%d:0x%x\n", Nr, phdr->p_offset);
 		}
 
         return 0;
