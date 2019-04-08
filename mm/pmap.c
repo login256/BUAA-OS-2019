feeffcfacf215010143e6cb320573805bce59cd1
login256 <ahaloer@163.com>
2019-04-08 16:23:27 +0800

    modify

--- mm/pmap.c
+++ mm/pmap.c
@@ -29,12 +29,13 @@ u_long cal_page(int taskKind, u_long va, int n, Pde *pgdir)
 	}
 	else if (taskKind == 2)
 	{
-		return ((va>>22)<<22)+n*4096;
+		return ((va>>22)<<22)+(n-1)*4096;
 	}
 	else if (taskKind == 3)
 	{
-		x = (va-((va>>22)<<22))>>12;
-		pgdir_entry = pgdir+x;
+		//x = (va-((va>>22)<<22))>>12;
+		x=va>>22;
+		pgdir_entry = &pgdir[x];
 		*pgdir_entry = (PADDR(pgdir))|PTE_V;
 		return 0;
 	}
