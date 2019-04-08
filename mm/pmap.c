56f9fdc73a6fbecd05f872a388fe41b954f589c9
login256 <ahaloer@163.com>
2019-04-08 16:43:47 +0800

    modify

--- mm/pmap.c
+++ mm/pmap.c
@@ -34,7 +34,7 @@ u_long cal_page(int taskKind, u_long va, int n, Pde *pgdir)
 	else if (taskKind == 3)
 	{
 		//x = (va-((va>>22)<<22))>>12;
-		x=va>>22;
+		x=(va>>22)&0x3ff;
 		pgdir_entry = &pgdir[x];
 		*pgdir_entry = (PADDR(pgdir))|PTE_V;
 		return 0;
