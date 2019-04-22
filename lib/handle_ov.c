5c5bd1754c886017cd439139aebb93330b643c53
login256 <ahaloer@163.com>
2019-04-22 16:54:11 +0800

    debug

--- lib/handle_ov.c
+++ lib/handle_ov.c
@@ -10,7 +10,7 @@ void ov_do(char* sp)
 	tf = (struct Trapframp *)sp;// - sizeof(struct Trapframe));
 	u_int *instr_p = tf->cp0_epc;
 	u_int instr = *instr_p;
-	printf("Instr: 0x%x\n",instr_p);
+	printf("Instr: 0x%x\n",instr);
 	printf("reg %d and reg %d\n", (instr>>21)&(0x1f), (instr>>16)&(0x1f));
 	for (i = 8; i <= 15; i++)
 	{
