66f7f891553d3d091ffb76330aae76c6f0ad001e
10.210.0.67 <10.210.0.67@example.com>
2019-05-20 15:13:10 +0800

    Judgement for lab5-2-Extra at 2019-05-20T15:13:10+0800

--- include/kclock.h
+++
@@ -1,9 +0,0 @@
-/* See COPYRIGHT for copyright information. */
-
-#ifndef _KCLOCK_H_
-#define _KCLOCK_H_
-#define	IO_RTC		0xb5000100		/* RTC port */
-#ifndef __ASSEMBLER__
-void kclock_init(void);
-#endif /* !__ASSEMBLER__ */
-#endif
