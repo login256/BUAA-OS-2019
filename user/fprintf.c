3ae5800c19ef8d9deff424cee6864ca0bbe5d4b5
login256 <ahaloer@163.com>
2019-05-27 12:45:24 +0800

    modify fprintf and confirm

--- user/fprintf.c
+++ user/fprintf.c
@@ -5,24 +5,25 @@ static int curlen;
 static void user_out2string(void *arg, char *s, int l)
 {
     int i;
-	char * b = (char *)arg /*+ curlen*/;
+	char * b = (char *)arg + curlen;
     // special termination call
     if ((l==1) && (s[0] == '\0')) return;
     
     for (i=0; i< l; i++) {
-		b[curlen++]=s[i];
+		b[i]=s[i];
     }
-	//curlen += l;
+	curlen += l;
 }
 
 
 int fwritef(int fd, const char *fmt, ...)
 {
-	char buf[512] = {0};
+	char buf[512];
 	va_list ap;
 	curlen = 0;
 	va_start(ap, fmt);
 	user_lp_Print(user_out2string, buf, fmt, ap);
 	va_end(ap);
+	buf[curlen] = '\0';
 	return write(fd, buf, strlen(buf));
 }
